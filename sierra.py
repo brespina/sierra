cb78526e8d4a998b6fc808d05ed85263bc4cd76e
brespina <brespina4@gmail.com>
2024-10-10 21:54:05 -0500

    added free/premium filtering as required argument

--- sierra.py
+++ sierra.py
@@ -22,17 +22,34 @@ with open('question_bank/leetcode_problems.json', 'r') as f:
    
 def fetch_problems():
     return leetcode_data['stat_status_pairs'] 
+
+
+def filter_problems(problems):
+    free_problems = []
+    for p in problems:
+        if p['paid_only'] == False:
+            free_problems.append(p)
+                    
+    return free_problems
  
  
 def get_embed_color(question_data):
-    if question_data['difficulty']['level'] == '3':
+    if question_data['difficulty']['level'] == 3:
         embed_color = discord.Color.red()
-    elif question_data['difficulty']['level'] == '2':
+    elif question_data['difficulty']['level'] == 2:
         embed_color = discord.Color.orange()
     else:
         embed_color = discord.Color.green()
 
     return embed_color
+
+def format_difficulty(difficulty):
+    if difficulty['level'] == 3:
+        return 'hard'
+    elif difficulty['level'] == 2:
+        return 'medium'
+    else:
+        return 'easy'
 
         
 @bot.event
@@ -46,14 +63,20 @@ async def on_ready():
 
 
 @bot.tree.command(name="leetcode")
-async def leetcode(interaction: discord.Interaction):
+async def leetcode(interaction: discord.Interaction, include_premium: bool):
     # grab random q from my local folder
     try:
         problems = fetch_problems() 
+        # if user says no premium then filter only free questions for that user.
+        if include_premium == False:
+            problems = filter_problems(problems)            
+                    
         random_problem = random.choice(problems)
         title = random_problem['stat']['question__title']
         difficulty = random_problem['difficulty']
         slug =  random_problem['stat']['question__title_slug']
+        
+        difficulty_formatted = format_difficulty(difficulty)
 
         problem_url = f"https://leetcode.com/problems/{slug}"
 
@@ -65,7 +88,7 @@ async def leetcode(interaction: discord.Interaction):
             url=problem_url,
             color=embed_color
         )
-        embed.add_field(name="difficulty", value=difficulty, inline=False)
+        embed.add_field(name="difficulty", value=difficulty_formatted, inline=False)
 
         
         await interaction.response.send_message(embed=embed)
